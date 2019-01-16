d08b9780c422223778891b27790a7cd1b840c7b2
Ivan Kuchukov <ivan.kuchukov@gmail.com>
2019-01-16 21:19:57 +0300

    modify HTMLHelper::jsBlock

--- models/HtmlHelper.php
+++ models/HtmlHelper.php
@@ -10,22 +10,43 @@ use trivial\controllers\App;
  */
 class HtmlHelper {
     
+    private static $js = [];
+    private static $css = [];
+
     /**
      * jsRequire
      * @param array $jsRequire
      */
     public static function jsRequire(array $jsRequire=[]) {
-        foreach ($jsRequire as $jsRequireType) {
+        self::$js[] = array_merge(self::$js[],$jsRequire);
+    }
+    
+    /**
+     * jsBlock
+     * @param array $jsRequire
+     */
+    public static function jsBlock(array $jsRequire=[]) {
+        self::jsRequire($jsRequire);
+        foreach (array_unique(self::$js) as $jsRequireType) {
             echo App::params("includedJavascripts.".$jsRequireType);
         }
     }
     
     /**
      * cssRequire
-     * @param array $jsRequire
+     * @param array $cssRequire
      */
     public static function cssRequire(array $cssRequire=[]) {
-        foreach ($cssRequire as $cssRequireType) {
+        self::$css[] = array_merge(self::$css[],$cssRequire);
+    }
+    
+    /**
+     * cssBlock
+     * @param array $cssRequire
+     */
+    public static function cssBlock(array $cssRequire=[]) {
+        self::cssRequire($cssRequire);
+        foreach (array_unique(self::$css) as $cssRequireType) {
             echo App::params("includedCSS.".$cssRequireType);
         }
     }
